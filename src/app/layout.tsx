0d0155762049efa6eb40f2c69297f263cb81e886
thammarongsak <134133829+thammarongsak@users.noreply.github.com>
2025-09-06 02:01:01 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,12 @@
+export const metadata = { title: "Waibon Safe Room" };
+
 import "./globals.css";
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="th">
+      <body>{children}</body>
+    </html>
+  );
+}
