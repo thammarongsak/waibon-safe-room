6e5a118b3db7df13ca796988aca2ab2ac36e50fe
thammarongsak <134133829+thammarongsak@users.noreply.github.com>
2025-09-07 03:43:56 +0700

    Update config.ts

--- src/lib/zeta/v10/config.ts
+++ src/lib/zeta/v10/config.ts
@@ -1,5 +1,5 @@
 // lib/zeta/v10/config.ts
-import fs from "fs";
+//import fs from "fs";
 export type UnifiedCore = any;
 export const unifiedCore: UnifiedCore = JSON.parse(
   fs.readFileSync(process.cwd()+"/config/WaibonOS_Unified_Core_v10.json","utf8")
