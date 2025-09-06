1274f3bcbe49adaaabfbd44debd2b22a43ed0a04
thammarongsak <134133829+thammarongsak@users.noreply.github.com>
2025-09-07 04:13:05 +0700

    Delete src/lib/zeta/v10 directory

--- src/lib/zeta/v10/config.ts
+++
@@ -1,6 +0,0 @@
-// lib/zeta/v10/config.ts
-//import fs from "fs";
-//export type UnifiedCore = any;
-//export const unifiedCore: UnifiedCore = JSON.parse(
-  //fs.readFileSync(process.cwd()+"/config/WaibonOS_Unified_Core_v10.json","utf8")
-//);
--- src/lib/zeta/v10/index.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/zeta/v10/index.ts
-export * from "./adapter";
-export * from "./core";
-export * from "./memory";
-export * from "./persona";
-export * from "./transport";
-
