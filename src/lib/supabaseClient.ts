e09bd8ea752fca2bad95fabadb6feabd781544d7
thammarongsak <134133829+thammarongsak@users.noreply.github.com>
2025-09-09 01:56:52 +0700

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 import { ENV } from './env';
-export const supabaseClient = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_ANON_KEY);
 
+export const supabaseClient = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_ANON_KEY);
