07488bf7bd065a49b7add8554d1c8aa6b077a92e
thammarongsak <134133829+thammarongsak@users.noreply.github.com>
2025-09-09 02:31:49 +0700

    Update supabaseServer.ts

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -2,6 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { ENV } from './env';
 
-export const supabaseServer = createClient(ENV.SUPABASE_URL, ENV.SUPABASE_SERVICE_ROLE_KEY, {
-  auth: { persistSession: false },
-});
+export const supabaseServer = createClient(
+  ENV.SUPABASE_URL,
+  ENV.SUPABASE_SERVICE_ROLE_KEY,
+  { auth: { persistSession: false } }
+);
